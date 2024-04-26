0638160a99e822bf445b8c328e85888d5c594dbc
CodeGoutam <hgoutam2001@gmail.com>
2024-04-26 18:08:06 +0530

    updates

--- backend/name.js
+++ backend/name.js
@@ -6,11 +6,11 @@ const nameSchema = require('./NameSchema')
 route.post("/name", async (req, res) => {
     console.log("api");
     await nameSchema.create({ name: req.body.name })
-    .then((result) => {
-        res.send({ success: true })
-    }).catch((err) => {
-        res.send({ success: false })
-    });
+        .then((result) => {
+            res.send({ success: true })
+        }).catch((err) => {
+            res.send({ success: false })
+        });
 
 })
 module.exports = route;
