c4a058cc43b6aefb162725dd7a7787201f7c739a
CodeGoutam <hgoutam2001@gmail.com>
2024-04-26 14:27:32 +0530

    path

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     const fun = async (e) => {
         e.preventDefault();
         try {
-            const res = await fetch("http://localhost:5000/api/name", {
+            const res = await fetch("https://to-deploy-2.onrender.com/api/name", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ name: name })
